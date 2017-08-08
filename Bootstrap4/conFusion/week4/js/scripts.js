caa8fc2ff10263c2f15076be43fc92c2a65959e7
jasonpeng <peng.jason03265@gmail.com>
2017-08-08 00:42:18 -0700

    npm scripts

---
+++ Bootstrap4/conFusion/week4/js/scripts.js
@@ -0,0 +1,19 @@
+
+    $(document).ready(function(){
+        $("#mycarousel").carousel({interval:1000});
+        $("#carousel-button").click(function(){
+            if($("#carousel-button").children("span").hasClass("fa-pause")){
+                $("#mycarousel").carousel('pause');
+                $("#carousel-button").children("span").removeClass("fa-pause");
+                $("#carousel-button").children("span").addClass("fa-play");
+
+            }
+            else{
+                $("#mycarousel").carousel('cycle');
+                $("#carousel-button").children("span").removeClass("fa-play");
+                $("#carousel-button").children("span").addClass("fa-pause");
+            }
+        });
+      
+
+    })    
